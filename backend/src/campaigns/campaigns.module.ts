d94881c55eefbe382cde46d31105ef60744dd417
devtayyab <m.tayyab@peter-park.de>
2025-09-23 16:44:59 +0500

    fix: sqs service

--- backend/src/campaigns/campaigns.module.ts
+++ backend/src/campaigns/campaigns.module.ts
@@ -4,11 +4,12 @@ import { CampaignsController } from './campaigns.controller';
 import { CampaignsService } from './campaigns.service';
 import { Campaign } from './entities/campaign.entity';
 import { EmailJob } from '../email-jobs/entities/email-job.entity';
+import { SqsService } from '@/sqs/sqs.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Campaign, EmailJob])],
   controllers: [CampaignsController],
-  providers: [CampaignsService],
+  providers: [CampaignsService,SqsService],
   exports: [CampaignsService],
 })
 export class CampaignsModule {}
